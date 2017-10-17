ec54f00d5776f512ba2b7aae7f14b7343b051da5
IanJeffery1999 <iancjeffery@gmail.com>
2017-10-17 10:07:40 -0400

    Ian Jeffery news modal commit
    news modal

--- js/init.js
+++ js/init.js
@@ -6,3 +6,19 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal1').modal();
+  });
+     
+$('.modal').modal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      inDuration: 300, // Transition in duration
+      outDuration: 200, // Transition out duration
+      startingTop: '4%', // Starting top style attribute
+      endingTop: '10%', // Ending top style attribute
+    
+       }
+  );
